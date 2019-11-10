6bd62b436ff8e7abd45d01e4f23693fabef493fa
Dyn4sty <liorkasha17@gmail.com>
2019-11-10 22:09:14 +0200

    Updates

---
+++ precache-manifest.cbaa22254f8d793ecd9f9e080017da91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f3057c18bf6c73e5f753204181ecc6",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "8e7dc37475c728c1edda",
+    "url": "/Pokedex/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "48c0cfbd42000edce195",
+    "url": "/Pokedex/static/css/main.86267bd4.chunk.css"
+  },
+  {
+    "revision": "8e7dc37475c728c1edda",
+    "url": "/Pokedex/static/js/2.5ba26726.chunk.js"
+  },
+  {
+    "revision": "48c0cfbd42000edce195",
+    "url": "/Pokedex/static/js/main.f3f6e1d0.chunk.js"
+  },
+  {
+    "revision": "6bd1269e53f820af2c87",
+    "url": "/Pokedex/static/js/runtime-main.7f4dd92b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Pokedex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
